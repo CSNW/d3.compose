9c476e8438b682ec0df84aae8d5ebbb26f8918a0
Tim Hall <tim.hall.engr@gmail.com>
2014-03-18 10:43:40 -0400

    Add comments and update value data

--- d3.chart.csnw.line-bar.js
+++ d3.chart.csnw.line-bar.js
@@ -1,7 +1,14 @@
 (function(d3, _) {
   'use strict';
 
+  /**
+   * Base
+   * 
+   * Common base that includes helpers for all charts
+   * ------------------------------------------------------- */
   d3.chart('Base', {
+    // TODO: Look at wrapping transform in initialization to store data() automatically
+
     width: function width() {
       return dimensions(this).width;
     },
@@ -13,11 +20,23 @@
         this.trigger('change:data', data);
       }
     }),
+
+    /**
+     * Helper for attaching charts object
+     * 
+     * @param {Object} charts with chart name as key
+     */
     attachCharts: function(charts) {
       _.each(charts, function(chart, name) {      
         this.attach(name, chart);
       }, this);
     },
+
+    /**
+     * Helper for attaching components object
+     *
+     * @param {Object} components with component name as key
+     */
     attachComponents: function(components) {
       _.each(components, function(component, name) {
         this.attach(name, component);
@@ -33,6 +52,16 @@
         });
       }, this);
     },
+
+    /**
+     * For each property, create combined setter that calls setter of each chart
+     * optionally, call setter from options after all setters have been called
+     *
+     * @param {Array} charts
+     * @param {Array} properties property names to create combined setter
+     * @param {Object} [options]
+     * - set: Setter to call after all chart setters
+     */
     wrapProperties: function(charts, properties, options) {
       options = options || {};
       _.each(properties, function(prop) {
@@ -49,7 +78,12 @@
       }, this);
     }
   });
-
+  
+  /**
+   * Container
+   *
+   * Chart container with helpers for placing charts, dimensioning, and redrawing
+   * ------------------------------------------------------- */
   d3.chart('Base').extend('Container', {
     initialize: function() {
       this.base.classed('chart', true);
@@ -65,30 +99,42 @@
     updateDimensions: function() {
       var bounds = this.bounds();
 
+      // Explicitly set width and height of container
       this.base
         .attr('width', this.width())
         .attr('height', this.height());
+
+      // Place chart base within container
       this.chartBase()
         .attr('transform', 'translate(' + bounds.left + ', ' + bounds.top + ')')
         .attr('width', bounds.width)
         .attr('height', bounds.height);
     },
+
     redraw: function() {
+      // Using previously saved rawData, redraw chart
       if (this.rawData())
         this.draw(this.rawData());
     },
 
     transform: function(data) {
+      // Container's transform is first to be called in chain
+      // so data that comes in here hasn't been through any transforms
+      // Save as raw data (as it was originally passed in) for use in redrawing
       this.rawData(data);
       return data;
     },
     rawData: property('rawData'),
 
     chartBase: property('chartBase'),
+
+    // TODO: Rename chartMargins(), split off chartWidth() and chartHeight(), and add update helper
     bounds: property('bounds', {
       get: function(values) {
         values = (values && typeof values == 'object') ? values : {};
         values = _.defaults(values, {top: 0, right: 0, bottom: 0, left: 0});
+
+        // Add width and height helpers
         values.width = this.width() - values.left - values.right;
         values.height = this.height() - values.top - values.bottom;
 
@@ -101,6 +147,7 @@
         return {
           override: values,
           after: function() {
+            // Trigger change dimensions "after" set so that override value is used
             this.trigger('change:dimensions');
           }
         };
@@ -124,18 +171,28 @@
     })
   });
 
+  /**
+   * XYBase
+   * 
+   * Chart base with helpers for handling (x, y) data
+   * - Expected data format: [{x, y, key}...]
+   * ------------------------------------------------------- */
   d3.chart('Base').extend('XYBase', {
     initialize: function() {
       this.on('change:data', this.setScales);
     },
 
+    // Get scaled x-value
     x: function(d, i) {
       return this.xScale()(this.xValue(d, i));
     },
+    
+    // Get scaled y-value
     y: function(d, i) {
       return this.yScale()(this.yValue(d, i));
     },
 
+    // Get x, y, and key from data
     xValue: function(d, i) {
       return d.x;
     },
@@ -147,17 +204,30 @@
     },
 
     setScales: function() {
+      // Use saved scales or create default (linear with from min-max)
       var xScale = this.xScale() || d3.scale.linear().domain([this.xMin(), this.xMax()]);
       var yScale = this.yScale() || d3.scale.linear().domain([this.yMin(), this.yMax()]);
 
+      // Range is dependent on chart dimensions and should be set separately
+      // (User may want to set domain explicity, but range changes as dimensions change)
       xScale = this.setXScaleRange(xScale, this.data() || [], this);
       yScale = this.setYScaleRange(yScale, this.data() || [], this);
       
       this.xScale(xScale).yScale(yScale);
     },
+
+    /**
+     * Set range for x-scale
+     * (Can be overriden based on chart needs)
+     */
     setXScaleRange: function(xScale, data, chart) {
       return xScale.range([0, chart.width()]);
     },
+
+    /**
+     * Set range for y-scale
+     * (Can be overriden based on chart needs)
+     */
     setYScaleRange: function(yScale, data, chart) {
       return yScale.range([chart.height(), 0]);
     },
@@ -191,20 +261,33 @@
     })
   });
   
+  /**
+   * ValueBase
+   *
+   * Extension of XYBase for set of values that uses index for x-value
+   * - Expected data format: [values...] or [{y, key}...]
+   * ------------------------------------------------------- */
   d3.chart('XYBase').extend('ValueBase', {
     transform: function(data) {
+      // Transform data from [values...] or [{y, key}...] into XYBase [{x, y, key}...]
       return data.map(function(item, index) {
-        var value = typeof item == 'object' ? item.value || item.y : item;
-        var key = typeof item == 'object' ? item.key || item.name : null;
+        var value = typeof item == 'object' ? item.y : item;
+        var key = typeof item == 'object' ? item.key : null;
         return {x: index, y: value, key: key};
       });
     }
   });
 
-  // TODO: Look at ordinal scale
-  // https://github.com/mbostock/d3/wiki/Ordinal-Scales
+  /**
+   * CenteredValueBase
+   *
+   * Extend ValueBase to center items at each data point
+   * ------------------------------------------------------- */
   d3.chart('ValueBase').extend('CenteredValueBase', {
+    // TODO: Look at ordinal scale
+    // https://github.com/mbostock/d3/wiki/Ordinal-Scales
     setXScaleRange: function(xScale, data, chart) {
+      // Include 1/2 itemPadding at each side
       var left = chart.itemWidth() / 2 + this.itemPadding() / 2;
       var right = chart.width() - chart.itemWidth() / 2 - this.itemPadding() / 2;
 
@@ -212,6 +295,7 @@
     },
 
     itemWidth: function(d, i) {
+      // Include an additional itemPadding to account for 1/2 itemPadding at each side
       var count = this.data() ? this.data().length : 0;
       if (count > 0)
         return (this.width() - this.itemPadding() * (count)) / count;
@@ -225,6 +309,13 @@
     })
   });
 
+  /**
+   * BarChart
+   *
+   * Vertical bar chart
+   * CSS: .bar-chart -> .bar
+   * TODO: Handle multiple series with clustered bars
+   * ------------------------------------------------------- */
   d3.chart('CenteredValueBase').extend('BarChart', {
     initialize: function() {
       this.layer('BarChart', this.base.append('g').classed('bar-chart', true), {
@@ -259,7 +350,14 @@
     }
   });
 
-  // Example: https://github.com/misoproject/d3.chart/issues/30
+  /**
+   * CenteredLineChart
+   *
+   * Line chart of y-values with x-value centered at index
+   * CSS: .line-chart -> .line
+   * TODO: Handle multiple series
+   * Example: https://github.com/misoproject/d3.chart/issues/30
+   * ------------------------------------------------------- */
   d3.chart('CenteredValueBase').extend('CenteredLineChart', {
     initialize: function() {
       var line = d3.svg.line()
@@ -286,10 +384,17 @@
     }
   });
 
+  /**
+   * CenteredDataLabelsChart
+   *
+   * Labels of y-values positioned at y-values with x-value centered at index
+   * CSS: .label-chart -> .label
+   * TODO: Add labels and multiple series (circles temporarily)
+   * ------------------------------------------------------- */
   d3.chart('CenteredValueBase').extend('CenteredDataLabelsChart', {
     // Add circles (temporarily)
     initialize: function() {
-      this.layer('PointChart', this.base.append('g').classed('point-chart', true), {
+      this.layer('PointChart', this.base.append('g').classed('label-chart', true), {
         dataBind: function(data) {
           var chart = this.chart();
           chart.data(data);
@@ -298,7 +403,7 @@
         },
         insert: function() {
           return this.append('circle')
-            .classed('point', true);
+            .classed('label', true);
         },
         events: {
           merge: function() {
@@ -313,6 +418,13 @@
     }
   });
 
+  /**
+   * Axis
+   * 
+   * Options:
+   * - position: top, right, bottom, left
+   * CSS: .axis
+   * ------------------------------------------------------- */
   d3.chart('XYBase').extend('Axis', {
     initialize: function() {
       this.axis = d3.svg.axis();
@@ -397,7 +509,11 @@
     container: property('container')
   });
   
-  // TODO: Update scale / ordinal scale
+  /**
+   * CenteredAxis : Axis
+   * 
+   * Axis with ticks centered at values
+   * ------------------------------------------------------- */
   d3.chart('Axis').extend('CenteredAxis', {
     initialize: function() {
       this.on('change:data', function(data) {
@@ -406,6 +522,11 @@
     }
   });
 
+  /**
+   * CenteredLineChartWithLabels
+   * 
+   * Combination of CenteredLineChart and CenteredDataLabelsChart
+   * ------------------------------------------------------- */
   d3.chart('CenteredValueBase').extend('CenteredLineChartWithLabels', {
     initialize: function() {
       this.charts = {
@@ -416,25 +537,13 @@
       this.attachCharts(this.charts);
       this.wrapProperties(this.charts, ['xScale', 'xMin', 'xMax', 'yScale', 'yMin', 'yMax', 'itemPadding']);
     }
-  })
-
-  d3.chart('Container').extend('SimpleBarChart', {
-    initialize: function() {
-      this.charts = {
-        'Bars': this.chartBase().chart('BarChart')
-      };
-
-      this.attachCharts(this.charts);
-      this.bounds({top: 10, right: 10, bottom: 40, left: 10});
-    },
-    itemPadding: property('itemPadding', {
-      set: function(value) {
-        this.charts['BarChart'].itemPadding(value);
-        this.trigger('change:dimensions');
-      }
-    })
   });
 
+  /**
+   * LineBarChart
+   * 
+   * Combination of BarChart and CenteredLineChartWithLabels with Axes
+   * ------------------------------------------------------- */
   d3.chart('Container').extend('LineBarChart', {
     initialize: function() {
       this.charts = {
@@ -475,7 +584,17 @@
       return data[name];
     }
   });
-
+  
+  /**
+   * Property helper
+   *
+   * @param {String} name of stored property
+   * @param {Object} options
+   * - get: function(value) {return ...} getter, where value is the stored value, return desired value
+   * - set: function(value, previous) {return {override, after}} 
+   *        setter, return override to set stored value and after() to run after set
+   * - type: 'function' if get/set value is function, otherwise get/set is evaluated if they're a function
+   */
   function property(name, options) {
     var prop_key = '__properties'
     options = options || {};
@@ -517,6 +636,7 @@
     };
   }
 
+  // Dimensions helper for robustly determining width/height of given chart
   function dimensions(chart) {
     var element = chart.base && chart.base.length && chart.base[0] && chart.base[0].length && chart.base[0][0];
 
