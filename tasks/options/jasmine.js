1399fededc2ddc6141bfb63ae269f8b2d0e409d9
Tim Hall <tim.hall.engr@gmail.com>
2014-03-26 13:04:17 -0400

    Save spec runner once generated
    After running `grunt test` spec runner can be found in specs/index.html

--- tasks/options/jasmine.js
+++ tasks/options/jasmine.js
@@ -2,11 +2,12 @@ module.exports = {
   src: '<%= meta.srcFiles %>',
   options: {
     specs: 'specs/**/*.spec.js',
-    helpers: 'specs/helpers/*.js',
     vendor: [
       'bower_components/d3/d3.js',
       'bower_components/underscore/underscore.js',
       'bower_components/d3.chart/d3.chart.js'
-    ]
+    ],
+    outfile: 'specs/index.html',
+    keepRunner: true
   }
 };
