7c78a913cb425f008ff1cf1086e962f62b66e7ff
Tim Hall <tim.hall.engr@gmail.com>
2015-02-20 17:03:03 -0500

    Update Base specs

--- specs/Multi.spec.js
+++ specs/Multi.spec.js
@@ -1,70 +1,208 @@
 (function(d3, _, helpers, mixins) {
 
   describe('Multi', function() {    
-    var data = {
-      a: [
-        {key: 'a1', values: [{x:0,y:100}, {x:25,y:200}, {x:50,y:300}, {x:75,y:500}]},
-        {key: 'a2', values: [{x:0,y:-100}, {x:25,y:-200}, {x:50,y:-300}, {x:100,y:-800}]}
-      ],
-      b: [
-        {key: 'b', values: [{x:-10,y:1}, {x:-20,y:2}, {x:-30,y:3}, {x:-40,y:4}]}
-      ],
-      c: [
-        {key: 'c', values: [{x:-10,y:1}, {x:-20,y:2}, {x:-30,y:3}, {x:-40,y:4}]}
-      ]
-    };
-    var values = {
-      a: [
-        {key: 'a1', values: [{x:'a',y:100}, {x:'b',y:200}, {x:'c',y:300}, {x:'d',y:500}]},
-        {key: 'a2', values: [{x:'a',y:-100}, {x:'b',y:-200}, {x:'c',y:-300}, {x:'e',y:-800}]}
-      ],
-      b: [
-        {key: 'b', values: [{x:'-a',y:1}, {x:'-b',y:2}, {x:'-c',y:3}, {x:'-d',y:4}]}
-      ],
-      c: [
-        {key: 'c', values: [{x:'-a',y:1}, {x:'-b',y:2}, {x:'-c',y:3}, {x:'-d',y:4}]}
-      ]
-    };
-    var chart, fixture, selection, configuration;
+    var fixture, selection, Container, container, Chart, charts, Component, components;
+    beforeEach(function() {
+      fixture = setFixtures('<svg id="chart"></svg>');
+      selection = d3.select('#chart');
 
-    function setupChart(config) {
-      chart = selection.chart('Multi', config);
+      Container = d3.chart('Multi').extend('TestContainer');
+      container = new Container(selection);
 
-      _.each(chart.axes(), function(axis) {
-        axis.setScales();
+      Chart = d3.chart('Chart').extend('TestChart');
+      Component = d3.chart('Component').extend('TestComponent');
+
+      charts = [];
+      components = [];
+    });
+
+    it('should attach chart', function() {
+      charts[0] = new Chart(container.chartLayer());
+      container.charts({
+        'chart1': charts[0]
       });
 
-      return chart;
-    }
-
-    beforeEach(function() {
-      fixture = setFixtures('<div id="chart"></div>');
-      selection = d3.select('#chart')
-        .append('svg');
-
-      configuration = {
-        type: 'Values',
-        charts: [
-          {type: 'Bars', dataKey: 'a'},
-          {type: 'Line', dataKey: 'b'}
-        ],
-        axes: {
-          x: {scale: {type: 'ordinal', domain: ['a', 'b', 'c'], roundRangeBands: [[0, 600]]}},
-          y: {scale: {domain: [0, 1000], range: [400, 0]}}
-        },
-        legend: {},
-        title: ''
-      };
-
-      setupChart(configuration);
+      expect(container.charts()['chart1']).toBe(charts[0]);
+      expect(container._attached['chart1']).toBe(charts[0]);
     });
 
-    describe('axes', function() {
+    it('should attach component', function() {
+      components[0] = new Component(container.componentLayer());
+      container.components({
+        component1: components[0]
+      });
       
+      expect(container.components()['component1']).toBe(components[0]);
+      expect(container._attached['component1']).toBe(components[0]);
     });
 
-    describe('data', function() {
-      
+    describe('Layout', function() {
+      var OverridenComponent, spy, chartSpy;
+      beforeEach(function() {
+        // Setup components
+        spy = jasmine.createSpy('component.draw');
+        Component = d3.chart('Component').extend('SimpleComponent', {
+          initialize: function() {
+            this.base.append('rect')
+              .attr('width', 50)
+              .attr('height', 100);
+          },
+          draw: spy
+        });
+        OverridenComponent = d3.chart('Component').extend('OverridenComponent', {
+          getLayout: function() {
+            return {
+              position: this.position(),
+              width: 60,
+              height: 70
+            };
+          },
+          initialize: function() {
+            this.base.append('rect')
+              .attr('width', 50)
+              .attr('height', 100);
+          },
+          draw: spy
+        });          
+
+        // Create and attach components
+        components = [
+          new Component(container.componentLayer({z_index: 101}), {position: 'top'}),
+          new OverridenComponent(container.componentLayer({z_index: 50}), {position: 'top'}),
+          new OverridenComponent(container.componentLayer({z_index: 50}), {position: 'right'}),
+          new Component(container.componentLayer({z_index: 100}), {position: 'right'}),
+          new Component(container.componentLayer({z_index: 1}), {position: 'bottom'}),
+          new OverridenComponent(container.componentLayer({z_index: 50}), {position: 'bottom'}),
+          new OverridenComponent(container.componentLayer({z_index: 50}), {position: 'left'}),
+          new Component(container.componentLayer({z_index: 200}), {position: 'left'})
+        ];
+
+        container.components({
+          'component-0': components[0],
+          'component-1': components[1],
+          'component-2': components[2],
+          'component-3': components[3],
+          'component-4': components[4],
+          'component-5': components[5],
+          'component-6': components[6],
+          'component-7': components[7]
+        });
+
+        // Setup and attach chart
+        chartSpy = jasmine.createSpy('chart.draw');
+        Chart = d3.chart('Chart').extend('SimpleChart', {
+          draw: chartSpy
+        });
+
+        container.charts({
+          'chart-1': new Chart(container.chartLayer())
+        });
+
+        // Setup container
+        container.width(600).height(400);
+        container.layout();
+
+        /*
+          Expected:
+          Chart: width x height = 600 x 400
+
+          Top
+          ---
+          0: offset width x height = 50 x 100 -> offset 100 -> 170 - 100 = 70 -> (110, 70)
+          1: 60 x 70 -> 70 -> 70 - 70 = 0 -> (110, 0)
+          Chart: 0 + 100 + 70 = 170
+          
+          Right
+          -----
+          2: 60 x 70 -> 60 -> 490 + 0 = 490 -> (490, 170)
+          3: 50 x 100 -> 50 -> 490 + 60 = 550 -> (550, 170)
+          Chart: Width - 60 - 50 = 490
+
+          Bottom
+          ------
+          4: 50 x 100 -> 100 -> 230 + 0 = 230 -> (110, 230)
+          5: 60 x 70 -> 70 -> 230 + 100 = 330 -> (110, 330)
+          Chart: Height - 100 - 70 = 230
+
+          Left
+          ----
+          6: 60 x 70 -> 60 -> 110 - 60 = 50 -> (50, 170)
+          7: 50 x 100 -> 50 -> 50 - 50 = 0 -> (0, 170)
+          Chart: 0 + 60 + 50 = 110
+
+          Chart: (110, 170), width = 380, height = 60
+        */
+      });
+
+      var translate = helpers.translate;
+
+      it('should layout "top" components by offset and position', function() {
+        expect(components[0].base.attr('transform')).toEqual(translate(110, 70));
+        expect(components[1].base.attr('transform')).toEqual(translate(110, 0));
+      });
+
+      it('should layout "right" components by offset and position', function() {
+        expect(components[2].base.attr('transform')).toEqual(translate(490, 170));
+        expect(components[3].base.attr('transform')).toEqual(translate(550, 170));
+      });
+
+      it('should layout "bottom" components by offset and position', function() {
+        expect(components[4].base.attr('transform')).toEqual(translate(110, 230));
+        expect(components[5].base.attr('transform')).toEqual(translate(110, 330));
+      });
+
+      it('should layout "left" components by offset and position', function() {
+        expect(components[6].base.attr('transform')).toEqual(translate(50, 170));
+        expect(components[7].base.attr('transform')).toEqual(translate(0, 170));
+      });
+
+      it('should layout chartBase by component layout', function() {
+        var chartLayer = container.base.select('.chart-layer');
+
+        expect(chartLayer.attr('transform')).toEqual(translate(110, 170));
+        expect(chartLayer.attr('transform')).toEqual(translate(110, 170));
+        expect(helpers.dimensions(chartLayer).width).toEqual(380);
+        expect(helpers.dimensions(chartLayer).height).toEqual(60);
+      });
+
+      // it('should pre-draw just components on draw', function() {
+      //   spyOn(d3.chart().prototype, 'draw');
+
+      //   container.draw([]);
+      //   expect(spy.calls.count()).toEqual(8);
+      //   expect(chartSpy.calls.count()).toEqual(0);
+      // });
+
+      it('should layout on draw', function() {
+        spyOn(container, 'layout');
+        spyOn(d3.chart().prototype, 'draw');
+
+        container.draw([]);
+        expect(container.layout).toHaveBeenCalled();
+      });
+
+      it('should set "z-index" with layering on draw', function() {
+        var expected = [
+          {'class': 'chart-component-layer', id: 'component-5', z_index: 1},
+          {'class': 'chart-component-layer', id: 'component-2', z_index: 50},
+          {'class': 'chart-component-layer', id: 'component-3', z_index: 50},
+          {'class': 'chart-component-layer', id: 'component-6', z_index: 50},
+          {'class': 'chart-component-layer', id: 'component-7', z_index: 50},
+          {'class': 'chart-component-layer', id: 'component-4', z_index: 100},
+          {'class': 'chart-layer', id: 'chart-1', z_index: 100},
+          {'class': 'chart-component-layer', id: 'component-1', z_index: 101},
+          {'class': 'chart-component-layer', id: 'component-8', z_index: 200}
+        ];
+
+        container.draw([]);
+
+        d3.select('.chart').selectAll('g').each(function(d, i) {
+          var selection = d3.select(this);
+          expect(selection.classed(expected[i]['class'])).toEqual(true);
+          expect(selection.attr('data-id')).toEqual(expected[i].id);
+          expect(+selection.attr('data-zIndex')).toEqual(expected[i].z_index);
+        });
+      });
     });
   });
 
---
+++ src/Base.js
@@ -0,0 +1,85 @@
+(function(d3, helpers) {
+  var each = helpers.utils.each;
+  var property = helpers.property;
+  
+  /**
+    Base
+    Shared functionality between all charts, components, and containers
+
+    Properties:
+    - data
+    - options
+    - style
+  */
+  d3.chart('Base', {
+    initialize: function() {
+      // Bind all di-functions to this chart
+      helpers.bindAllDi(this);
+    },
+
+    /**
+      Store fully-transformed data
+
+      @param {Object|Array} value
+    */
+    data: property('data'),
+
+    /**
+      General options for chart/component
+
+      @param {Object} value
+    */
+    options: property('options', {
+      default_value: {},
+      set: function(options) {
+        each(options, function(value, key) {
+          if (this[key] && this[key].is_property && this[key].set_from_options)
+            this[key](value);
+        }, this);
+      }
+    }),
+
+    /**
+      General style for chart/component
+
+      @param {Object} value
+    */
+    style: property('style', {
+      get: function(value) {
+        return helpers.style(value) || null;
+      }
+    }),
+    
+
+    /**
+      Get width/height of base
+    */
+    width: function width() {
+      return helpers.dimensions(this.base).width;
+    },
+    height: function height() {
+      return helpers.dimensions(this.base).height;
+    },
+
+    /**
+      Base is last transform to be called,
+      so stored data has been fully transformed
+    */
+    transform: function(data) {
+      data = data || [];
+
+      this.data(data);
+      return data;
+    },
+
+    /**
+      Add events to draw: before:draw and draw
+    */
+    draw: function(data) {
+      this.trigger('before:draw', data);
+      d3.chart().prototype.draw.apply(this, arguments);
+      this.trigger('draw', data);
+    }
+  });
+
+})(d3, d3.chart.helpers);
