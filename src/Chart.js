60496feebd0c50bc714c5c568bef62dddd1f2ef0
Tim Hall <tim.hall.engr@gmail.com>
2015-05-25 23:05:00 -0400

    Update docs (Chart)

--- src/Chart.js
+++ src/Chart.js
@@ -1,15 +1,62 @@
 (function(d3, charts) {
 
   /**
-    Foundation for building charts with series data
+    Common base for creating charts.
+    Standard `d3.chart` charts can be used with d3.compose, but extending `d3.chart('Chart')` includes helpers for properties and "di" functions.
 
+    ### Extending
+
+    To take advantage of "di"-binding (automatically injects `chart` into "di" methods)
+    and automatically setting properties from `options`, use `d3.compose.helpers.di`
+    and `d3.compose.helpers.property` when creating your chart.
+
+    @example
+    ```js
+    var helpers = d3.compose.helpers;
+
+    d3.chart('Chart').extend('Pie', {
+      initialize: function() {
+        // same as d3.chart
+      },
+      transform: function(data) {
+        // same as d3.chart
+      },
+
+      color: helpers.di(function(chart, d, i) {
+        // "di" function with parent chart injected ("this" = element)
+      }),
+
+      centered: helpers.property('centered', {
+        default_value: true
+        // can be automatically set from options object
+      })
+    });
+    ```
     @class Chart
+    @extends Base
   */
   charts.Chart = charts.Base.extend('Chart', {
     initialize: function(options) {
       this.options(options || {});
     }
   }, {
+    /**
+      Default z-index for chart
+      (Components are 50 by default, so Chart = 100 is above component by default)
+
+      @example
+      ```js
+      d3.chart('Chart').extend('BelowComponentLayers', {
+        // ...
+      }, {
+        z_index: 40
+      });
+      ```
+      @attribute z_index
+      @static
+      @type Number
+      @default 100
+    */
     z_index: 100,
     layer_type: 'chart'
   });
