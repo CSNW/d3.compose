202365cb5d0e192342e4f16f8d796c298367c456
Tim Hall <tim.hall.engr@gmail.com>
2015-07-08 17:26:49 -0400

    Refactor getting started

--- js/getting-started.js
+++ js/getting-started.js
@@ -1,7 +1,16 @@
 (function($, d3, examples, Editor) {
 
   $(document).ready(function() {
-    // Render masthead chart
+    var masthead = createMastheadChart();
+    var steps = createGettingStartedChart();
+
+    affix()
+    scrollspy(steps);
+
+    setActiveExample(steps);
+  });
+
+  function createMastheadChart() {
     var masthead = new Editor({
       el: '#home-chart'
     });
@@ -9,48 +18,60 @@
     masthead.setExample(examples['masthead']);
     masthead.render();
 
-    // Affix, scrollspy, and render getting started chart
+    return masthead;
+  }
+
+  function createGettingStartedChart() {
     var steps = new Editor({
       el: '#getting-started-chart',
       include_controls: false,
       dimensions: {width: 450, height: 340}
     });
 
-    var offset = $('#getting-started-chart').offset();
+    return steps;
+  }
 
+  function affix() {
+    var offset = $('#getting-started-chart').offset();
     $('.getting-started-chart-container').affix({
       offset: {
-        top: offset.top
+        top: offset.top - 50
       }
     });
+  }
 
+  function scrollspy(editor) {
     $(document.body).scrollspy({
       target: '.getting-started-nav',
       offset: 50
-    }).on('activate.bs.scrollspy', getActiveStep);
+    }).on('activate.bs.scrollspy', setActiveExample.bind(null, editor));
+  }
 
-    getActiveStep();
+  function setActiveExample(editor) {
+    var step = getActiveStep();
+    var example = examples['getting-started-' + step];
 
-    function getActiveStep() {
-      var active = $('.getting-started-nav').find('.active a')[0];
-      var step = 2;
-
-      if (active)
-        step = active.href.split('#')[1].split('-')[0];
-
-      if (step < 2)
-        return;
-
-      var example = examples['getting-started-' + step];
-
-      if (example) {
-        steps.setExample(example);
-        steps.render();
-      }
-      else {
-        console.error('No example found for step ' + step + ' (' + (active ? active.href : 'inactive') + ')');
-      }
+    if (example) {
+      editor.setExample(example);
+      editor.render();
     }
-  });
+    else {
+      console.error('No example found for step ' + step);
+    }
+  }
+
+  function getActiveStep() {
+    var active = $('.getting-started-nav').find('.active a')[0];
+    var step = 2;
+
+    if (active)
+      step = active.href.split('#')[1].split('-')[0];
+
+    // Start at step 2 by default (1 is empty)
+    if (step < 2)
+      step = 2;
+
+    return step;
+  }
 
 })(jQuery, d3, examples, Editor);
