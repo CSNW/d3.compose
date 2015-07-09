1d785f2a4629289e5b53126df25322b4589c2cca
Tim Hall <tim.hall.engr@gmail.com>
2015-07-09 16:15:01 -0400

    Add inline charts for responsive getting started

--- js/getting-started.js
+++ js/getting-started.js
@@ -2,12 +2,9 @@
 
   $(document).ready(function() {
     var masthead = createMastheadChart();
-    var steps = createGettingStartedChart();
 
-    affix()
-    scrollspy(steps);
-
-    setActiveExample(steps);
+    $(window).on('resize', _.throttle(showSteps, 100));
+    showSteps();
   });
 
   function createMastheadChart() {
@@ -21,7 +18,45 @@
     return masthead;
   }
 
-  function createGettingStartedChart() {
+  var shown_steps = false;
+  var shown_inline = false;
+  var hidden_steps = false;
+  var steps;
+
+  function showSteps() {
+    var show_steps = $('.getting-started-chart-container').is(':visible');
+
+    if (show_steps) {
+      if (shown_steps) {
+        if (hidden_steps) {
+          // Need to redraw after hide to make sure dimensions are up-to-date
+          hidden_steps = false;
+          steps.chart.redraw();  
+        }
+        
+        return;
+      }
+
+      shown_steps = true;
+      hidden_steps = false;
+      steps = createStepsChart();
+
+      affix()
+      scrollspy(steps);
+
+      setActiveExample(steps);
+    }
+    else {
+      hidden_steps = true;
+      if (shown_inline)
+        return;
+
+      shown_inline = true;
+      createInlineCharts();
+    }
+  }
+
+  function createStepsChart() {
     var steps = new Editor({
       el: '#getting-started-chart',
       include_controls: false,
@@ -65,20 +100,72 @@
 
   function getActiveStep() {
     var active = $('.getting-started-nav').find('.active a')[0];
-    var id, number;
+    var details;
 
     if (active) {
-      id = active.href.split('#')[1];
-      number = id.split('-')[0];
+      details = getDetailsFromLink(active);
     }
 
     // Start at step 2 by default (1 is empty)
-    if (!id || number < 2) {
-      id = '2-chart';
-      number = 2;
+    if (!details || details.number < 2) {
+      details = {
+        id: '2-chart',
+        number: 2
+      };
     }
 
-    return {number: number, id: id};
+    return details;
+  }
+
+  function createInlineCharts() {
+    var steps = loadExampleSteps();
+    _.each(steps, createInlineChart);
+  }
+
+  function loadExampleSteps() {
+    // Load ids from nav
+    var steps = [];
+    $('.getting-started-nav a').each(function(i) {
+      var details = getDetailsFromLink(this);
+
+      // Start at step 2 by default (1 is empty)
+      if (details.number >= 2)
+        steps.push(details);
+    });
+
+    return steps;
+  }
+
+  function getDetailsFromLink(link) {
+    var id = link.href.split('#')[1];
+    return {
+      id: id,
+      number: parseInt(id.split('-')[0])
+    };
+  }
+
+  function createInlineChart(step) {
+    var container = $('#' + step.id).parent().find('.chart-container');
+    var example = examples['getting-started-' + step.number];
+
+    if (!container.length) {
+      console.error('No chart container for step ' + JSON.stringify(step));
+      return;
+    }
+    if (!example) {
+      console.error('No example found for step ' + JSON.stringify(step));
+      return;
+    }
+
+    var chart = new Editor({
+      include_controls: false
+    });
+
+    container.empty();
+    container[0].appendChild(chart.el);
+
+    chart.setExample(example);
+    chart.render();
   }
 
 })(jQuery, d3, examples, Editor);
