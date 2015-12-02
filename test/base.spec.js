eb7a6846be75842cb79a81af4a28cd7afb75741f
Tim Hall <tim.hall.engr@gmail.com>
2015-12-02 16:56:58 -0500

    Use built-in Node 4.2 ES6 in tests

--- test/base.spec.js
+++ test/base.spec.js
@@ -2,24 +2,24 @@ import expect from 'expect';
 import Base from '../src/Base';
 import {property} from '../src/helpers';
 
-describe('Base', function() {
-  it('should set properties from options', function() {
-    var HasProperties = Base.extend({
+describe('Base', () => {
+  it('should set properties from options', () => {
+    const HasProperties = Base.extend({
       a: property()
     });
-    var base = new HasProperties();
+    const base = new HasProperties();
     base.options({a: 123, b: 456});
 
     expect(base.a()).toEqual(123);
     expect(base.options().b).toEqual(456);
   });
 
-  it('should clear existing properties for new options', function() {
-    var HasProperties = Base.extend({
+  it('should clear existing properties for new options', () => {
+    const HasProperties = Base.extend({
       a: property(),
       b: property()
     });
-    var base = new HasProperties();
+    const base = new HasProperties();
     base.options({a: 123, b: 456});
 
     expect(base.a()).toEqual(123);
@@ -31,8 +31,8 @@ describe('Base', function() {
     expect(base.b()).toEqual(undefined);
   });
 
-  it('should store fully-transformed data', function() {
-    var A = Base.extend({
+  it('should store fully-transformed data', () => {
+    const A = Base.extend({
       transform: function(data) {
         return data.map(function(value) {
           value += 10;
@@ -40,7 +40,7 @@ describe('Base', function() {
         });
       }
     });
-    var B = A.extend({
+    const B = A.extend({
       transform: function(data) {
         data = A.prototype.transform.call(this, data);
         return data.map(function(value) {
@@ -50,16 +50,16 @@ describe('Base', function() {
       }
     });
 
-    var base = new B();
+    const base = new B();
     base.draw([1, 2, 3]);
 
     expect(base.data()).toEqual([31, 32, 33]);
   });
 
-  it('should trigger before:draw and draw events on draw', function() {
-    var base = new Base();
-    var onBeforeDraw = expect.createSpy();
-    var onDraw = expect.createSpy();
+  it('should trigger before:draw and draw events on draw', () => {
+    const base = new Base();
+    const onBeforeDraw = expect.createSpy();
+    const onDraw = expect.createSpy();
 
     base.on('before:draw', onBeforeDraw);
     base.on('draw', onDraw);
--- test/setup.js
+++ test/setup.js
@@ -1,10 +1,10 @@
-import {jsdom} from 'jsdom';
-import d3 from 'd3';
-
 // Setup browser globals
+var jsdom = require('jsdom').jsdom;
 global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
 global.navigator = global.window.navigator;
 
+// Load d3 after jsdom to attach to document
 // d3.chart depends on global d3
+var d3 = require('d3');
 global.d3 = d3;
